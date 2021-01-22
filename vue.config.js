07737b710a1da4ad9515e21bb8de23c2de5b3d1e
Radek <radekwawrzyk@gmail.com>
2021-01-22 13:42:30 +0100

    Setup - Configure SCSS styles + remove unnecessary files

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: true,
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: '@import "~@/styles/modules/variables.scss";',
+      },
+    },
+  },
 };
