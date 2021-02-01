012476213ebc7e30570790b54997737e44276f75
Radek <radekwawrzyk@gmail.com>
2021-02-01 20:13:06 +0100

    Tests - BaseCalendar unit tests

---
+++ src/data/mockups.js
@@ -0,0 +1,142 @@
+export const days = [
+  {
+    date: '2020-12-28',
+    isCurrentMonth: false,
+  },
+  {
+    date: '2020-12-29',
+    isCurrentMonth: false,
+  },
+  {
+    date: '2020-12-30',
+    isCurrentMonth: false,
+  },
+  {
+    date: '2020-12-31',
+    isCurrentMonth: false,
+  },
+  {
+    date: '2021-01-01',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-02',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-03',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-04',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-05',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-06',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-07',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-08',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-09',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-10',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-11',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-12',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-13',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-14',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-15',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-16',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-17',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-18',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-19',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-20',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-21',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-22',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-23',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-24',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-25',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-26',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-27',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-28',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-29',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-30',
+    isCurrentMonth: true,
+  },
+  {
+    date: '2021-01-31',
+    isCurrentMonth: true,
+  },
+];
